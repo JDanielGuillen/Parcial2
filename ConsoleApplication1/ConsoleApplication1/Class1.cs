49a840c3869a00e787eea1a31e008812c7b9c402
JDanielGuillen <48265446+JDanielGuillen@users.noreply.github.com>
2019-03-19 19:41:47 -0600

    Add files via upload

---
+++ ConsoleApplication1/ConsoleApplication1/Class1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Class1
+    {
+        int n1, n2 = 0, totalS, e=1 ;
+        string[] nombres = new string[6];
+        int[] edad = new int[6];
+
+
+
+        public void primer()
+        {
+            Console.WriteLine("Ingrese el primer valor: ");
+            n1 = int.Parse(Console.ReadLine());
+            if (n1 != 0)
+            {
+                Console.WriteLine("Ingrese el segundo valor: ");
+                n2 = int.Parse(Console.ReadLine());
+                totalS = n1 * n2;
+                Console.WriteLine("El total es: " + totalS);
+            }
+            else if (n1 == 0)
+            {
+                Console.WriteLine("El producto de 0 por cualquier numero es 0 ");
+            }
+
+        }
+        public void segundo()
+        {
+            Console.WriteLine("Ingrese el primer valor: ");
+            n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el segundo valor: ");
+            n2 = int.Parse(Console.ReadLine());
+            if (n2 != 0)
+            {
+                
+                totalS = n1 / n2;
+                Console.WriteLine("El total es: " + totalS);
+            }
+            else if (n2 == 0)
+            {
+                Console.WriteLine("Error no se puede dividir entre cero ");
+            }
+
+        }
+        public void tercer()
+        {
+            for (int i = 1; i < 6; i++)
+            {
+                Console.WriteLine("Ingrese el " + i + ") nombre: ");
+                nombres[i] = Console.ReadLine();
+            }
+            for (int i = 1; i < 6; i++)
+            {
+                Console.WriteLine(i + ")" + nombres[i]);
+
+            }
+
+        }
+        public void cuarto()
+        {
+            do
+            {
+                
+
+                Console.WriteLine("Ingrese la " + e + ") edad : ");
+                edad[e] = int.Parse(Console.ReadLine());
+                e = e + 1;
+
+            } while (e < 6);
+            for (int i = 1; i < 6; i++)
+            {
+                Console.WriteLine(i + ") edad: " + edad[i]);
+            }
+        }
+
+    }
+}
---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+
+            Class1 obj = new Class1();
+            Console.WriteLine("Ingrese un valor para inciar el proceso: ");
+            Console.WriteLine("1) Para multipicar dos enteros: ");
+            Console.WriteLine("2) Para dividr dos numeros enteros: ");
+            Console.WriteLine("3) Para capturar 5 nombres: ");
+            Console.WriteLine("4) Para capturar 5 edades: ");
+            int N = int.Parse(Console.ReadLine());
+            if (N == 1)
+            {
+                obj.primer();
+            }
+            else if(N==2)
+            {
+                obj.segundo();
+            }
+            else if (N==3)
+            {
+                obj.tercer();
+            }
+            else if (N == 4)
+            {
+                obj.cuarto();
+            }
+            else
+            { Console.WriteLine("A ingresado un numero no valido "); }
+
+            Console.ReadKey();
+        }
+    }
+}
